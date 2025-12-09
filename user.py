4572e3921bff1e2cbd8f075e3b0dfb6a610a7182
ayousocrazy <ayousocrazy@gmail.com>
2025-12-09 13:06:50 +0545

    Add initial user class

---
+++ user.py
@@ -0,0 +1,21 @@
+import os
+os.chdir(r"C:\Users\HP\Desktop\Project\Inventory and Billing Management System")
+
+class Cashier:
+    def __init__(self, username, password, created_at):
+        self.username = username
+        self.__password = password
+        self.created_at = created_at
+        self.save_user()
+
+    def save_user(self):
+        role = "admin" if type(self).__name__.lower() == "admin" else "cashier"
+
+        with open("billing/users.txt", 'a') as doc:
+            doc_list = f"{self.username} {self.__password} {role} {self.created_at}\n"
+            doc.write(doc_list)
+
+
+class Admin(Cashier):
+    def __init__(self, username, password, created_at):
+        super().__init__(username, password, created_at)
